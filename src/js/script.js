a6f5dc76a671cf91eaf6b79b441c95d3b296380c
Radmir <uzyanbaevradmir@gmail.com>
2023-07-11 12:50:40 +0500

    all is ready

--- src/js/script.js
+++ src/js/script.js
@@ -48,4 +48,75 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn();
 		});
 	});
+
+	//form
+	function valideForms(form){
+		$(form).validate({
+			rules: {
+				name: {	
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите более {0} символов!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+				  required: "Пожалуйста, введите свою почту",
+				  email: "Неверный формат почты"
+				}
+			  }
+		});
+	};
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+        e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+	// Smooth scroll and pageup
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1000) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+	new WOW().init();
 });
